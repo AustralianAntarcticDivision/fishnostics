bbf2e7af214fca7b9f629a9245e6dbd92171d574
Paul Burch <Paul.Burch@aad.gov.au>
2017-02-07 09:13:34 +1100

    first commit

---
+++ R/fishnostics.R
@@ -0,0 +1,6 @@
+#' fishnostics: Diagnostics for fisheries assessment models, specifically CASAL,
+#' however, applicable to other fisheries assessments
+#' @docType package
+#' @name fishnostics
+NULL
+#> NULL
