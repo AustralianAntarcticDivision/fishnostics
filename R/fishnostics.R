386108ed17f4dffcb3669277e5566130339e1cd5
Paul Burch <Paul.Burch@aad.gov.au>
2017-02-20 14:36:42 +1100

    package description

--- R/fishnostics.R
+++ R/fishnostics.R
@@ -1,6 +1,29 @@
 #' fishnostics: Diagnostics for fisheries assessment models, specifically CASAL,
 #' however, applicable to other fisheries assessments
+#'
+#' This package currently includes plots used to present model outputs along
+#' with diagnostic plots.
+#'
+#' The intention is that this package will be implemented in completely in
+#' ggplot2, however, currently base and lattice are used.
+#'
+#' Need to generate some example datsets to use for package examples
+#'
 #' @docType package
 #' @name fishnostics
+NULL
+#> NULL
+
+#' Generic parameters for plotting
+#'
+#' @param probs quantile probabilities for plotting
+#' @param xlab x-axis label
+#' @param ylab y-axis label
+#' @param save logical to specify whether the plot should be saved (if TRUE a
+#' file_name must be supplied)
+#' @param file_name valid file name list to store model results
+#' @param file_type default = png
+#' @param graph_pars graphical parameters passed to par
+#' @name plotting
 NULL
 #> NULL
